f019e63177b859704d16b4c7d0660248964800bf
Enrique Ruiz <766685@celes.unizar.es>
2020-11-12 15:36:39 +0100

    Csv in progress

--- src/main/java/urlshortener/web/UrlShortenerController.java
+++ src/main/java/urlshortener/web/UrlShortenerController.java
@@ -27,8 +27,7 @@ public class UrlShortenerController {
   }
 
   @RequestMapping(value = "/{id:(?!link|index).*}", method = RequestMethod.GET)
-  public ResponseEntity<?> redirectTo(@PathVariable String id,
-                                      HttpServletRequest request) {
+  public ResponseEntity<?> redirectTo(@PathVariable String id, HttpServletRequest request) {
     ShortURL l = shortUrlService.findByKey(id);
     if (l != null) {
       clickService.saveClick(id, extractIP(request));
@@ -40,17 +39,18 @@ public class UrlShortenerController {
 
   @RequestMapping(value = "/link", method = RequestMethod.POST)
   public ResponseEntity<ShortURL> shortener(@RequestParam("url") String url,
-                                            @RequestParam(value = "sponsor", required = false)
-                                                String sponsor,
-                                            HttpServletRequest request) {
-    UrlValidator urlValidator = new UrlValidator(new String[] {"http",
-        "https"});
+      @RequestParam(value = "sponsor", required = false) String sponsor, HttpServletRequest request) {
+    UrlValidator urlValidator = new UrlValidator(new String[] { "http", "https" });
+    System.out.print("Received url " + url);
     if (urlValidator.isValid(url)) {
+      System.out.println(" - success");
+
       ShortURL su = shortUrlService.save(url, sponsor, request.getRemoteAddr());
       HttpHeaders h = new HttpHeaders();
       h.setLocation(su.getUri());
       return new ResponseEntity<>(su, h, HttpStatus.CREATED);
     } else {
+      System.out.println(" - error");
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
   }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -84,6 +84,123 @@ $(document).ready(function () {
 
     });
 
-    // event.preventDefault();
+
+    $("#csvButton").click(function (event) {
+        var csv = $('#filename');
+        var csvFile = csv[0].files[0];
+        var ext = csv.val().split(".").pop().toLowerCase();
+
+        if($.inArray(ext, ["csv"]) === -1){
+            alert('Please upload csv');
+            return false;
+        }
+        if(csvFile != undefined){
+            reader = new FileReader();
+            reader.onload = function(e){
+
+                payload = {
+                    "client": {
+                        "clientId": "urlshortener-295117",
+                        "clientVersion": "1.5.2"
+                    },
+                    "threatInfo": {
+                        "threatTypes": [
+                            "THREAT_TYPE_UNSPECIFIED",
+                            "MALWARE",
+                            "SOCIAL_ENGINEERING",
+                            "UNWANTED_SOFTWARE",
+                            "POTENTIALLY_HARMFUL_APPLICATION"
+                        ],
+                        "platformTypes": ["ANY_PLATFORM"],
+                        "threatEntryTypes": ["URL"],
+                        "threatEntries": []
+                    }
+                };
+
+
+                csvResult = e.target.result.split("\n");
+
+                console.log(`CSV result ${csvResult}`);
+
+                for (let i = 0; i < csvResult.length; i++) {
+                    payload.threatInfo.threatEntries[i] = { url : csvResult[i] };    
+                };
+
+                
+
+                //console.log(`La payload ${JSON.stringify(payload)}`);
+
+                $.ajax({
+                    type: "POST",
+                    url: safeBrowsingUrl,
+                    data: JSON.stringify(payload),
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    dataType: 'json',
+                    beforeSend: function () {
+                        $("#safeBrowsingCheck").text("Checking all URLs against Google safe browsing ...");
+                    },
+                    success: function (output, status, xhr) {
+        
+                        console.log(`Output: ${
+                            JSON.stringify(output)
+                        } and status ${status}`);
+        
+                        if (JSON.stringify(output) == "{}") {
+                            $("#safeBrowsingCheck").text("These websites are verified by google safe browsing ✅");
+                            $("#notifyShortening").text("Shortening urls ...");
+
+
+                            var shortenedUrls = "";
+
+                            for (let i = 0; i < csvResult.length; i++) {
+                                
+                                console.log(`Sending url ${csvResult[i]}`);
+                                $.ajax({
+                                    type: "POST",
+                                    url: "/link",
+                                    data: {
+                                        "url" : csvResult[i]
+                                    },
+                                    async: false,
+                                    success: function (msg) {
+                                            console.log("Success");
+                                            if (i == csvResult.length) {
+                                                shortenedUrls = shortenedUrls.concat(msg.uri);
+                                            } else{
+                                                shortenedUrls = shortenedUrls.concat(msg.uri,",");
+                                            }
+                                    },
+                                    error: function () {
+                                        console.log("Error en peticion");
+                                        $("#result").html("<div class='alert alert-danger lead'>ERROR</div>");
+                                    }
+                                });
+                            }
+
+                            console.log(`Shortened urls ${shortenedUrls}`);
+                                
+                          
+                        } else {
+                            $("#safeBrowsingCheck").text("Can't shorten some url because it is marked as unsafe by Google Safe Browsing ❌");
+                        } 
+        
+        
+                    },
+                    error: function (params) {
+                        alert("Error en peticion");
+                    }
+        
+        
+                });
+
+
+                
+            }
+            //Asyncronous
+            reader.readAsText(csvFile);
+        }
+    });
 
 });
