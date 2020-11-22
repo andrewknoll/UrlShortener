e5f25bd1902f68e66e61b88af3d2103288e1f32c
Enrique Ruiz <766685@celes.unizar.es>
2020-11-22 21:11:54 +0100

    Safebrowsing and csv implemented

--- bin/main/static/js/app.js
+++ bin/main/static/js/app.js
@@ -2,205 +2,167 @@ $(document).ready(function () {
 
     var safeBrowsingUrl = "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyD2-m3JAvdEYiAzPLhF-uVN2ZUIW6MkXU4";
 
+
+    // https://mrnxajqdmrmdwkrpenecpvmkozeusfipwpgw-dot-offgl8876899977678.uk.r.appspot.com/xxx
+
+
     $("#searchButton").click(function (event) {
+        $("#safeBrowsingCheck").text("");
+        $("#result").html("<div</div>");
+        var introducedUrl = $('#urlInput').val();
+        if (introducedUrl == "") {
+            $("#safeBrowsingCheck").text("Please insert a url !👆🏼");
+            return false;
+        }
 
-        var originalUrl = $('#urlInput').val();
-
-        console.log(`Original url: ${originalUrl}`);
-
-        payload = {
-            "client": {
-                "clientId": "urlshortener-295117",
-                "clientVersion": "1.5.2"
-            },
-            "threatInfo": {
-                "threatTypes": [
-                    "THREAT_TYPE_UNSPECIFIED",
-                    "MALWARE",
-                    "SOCIAL_ENGINEERING",
-                    "UNWANTED_SOFTWARE",
-                    "POTENTIALLY_HARMFUL_APPLICATION"
-                ],
-                "platformTypes": ["ANY_PLATFORM"],
-                "threatEntryTypes": ["URL"],
-                "threatEntries": [
-                    {
-                        "url": originalUrl
-                    }
-                ]
-            }
-        };
-
-
-        // Google safe browsing call
         $.ajax({
             type: "POST",
-            url: safeBrowsingUrl,
-            data: JSON.stringify(payload),
-            headers: {
-                "Content-Type": "application/json"
+            url: "/safeBrowsing",
+            data: {
+                "url": introducedUrl
             },
-            dataType: 'json',
-            beforeSend: function () {
-                $("#safeBrowsingCheck").text("Checking URL against Google safe browsing ...");
+            success: function (msg) {
+                console.log("Success");
+                $("#safeBrowsingCheck").text("This website is verified by google safe browsing ✅");
+                $("#notifyShortening").text("Shortening url ... ⏳");
+
+                $.ajax({
+                    type: "POST",
+                    url: "/link",
+                    data: {
+                        "url": introducedUrl
+                    },
+                    success: function (msg) {
+                        $("#result").html("<div class='alert alert-success lead'><a target='_blank' href='" + msg.uri + "'>" + msg.uri + "</a></div>");
+                        $("#notifyShortening").text("");
+                        $('#urlInput').val('');
+                    },
+                    error: function () {
+                        $("#result").html("<div class='alert alert-danger lead'>ERROR</div>");
+                    }
+                });
             },
-            success: function (output, status, xhr) {
+            error: function (output, status, xhr) {
 
-                console.log(`Output: ${
-                    JSON.stringify(output)
-                } and status ${status}`);
-
-                if (JSON.stringify(output) == "{}") {
-                    $("#safeBrowsingCheck").text("This website is verified by google safe browsing ✅");
-                    $("#notifyShortening").text("Shortening url ...");
-                    var url = $('#urlInput').val();
-                    console.log(`Sending url: ${url} as data`);
-                    $.ajax({
-                        type: "POST",
-                        url: "/link",
-                        data: {
-                            "url": url
-                        },
-                        success: function (msg) {
-                            $("#result").html("<div class='alert alert-success lead'><a target='_blank' href='" + msg.uri + "'>" + msg.uri + "</a></div>");
-                            $('#urlInput').val('');
-                        },
-                        error: function () {
-                            $("#result").html("<div class='alert alert-danger lead'>ERROR</div>");
-                        }
-                    });
+                if (output.status == 404) {
+                    $("#safeBrowsingCheck").text("Sorry, Google Safe Browsing couldn't be reached 😔");
+                } else if (output.status == 400) {
+                    $("#safeBrowsingCheck").text("Bad request, please check inserted url 🤨");
                 } else {
-                    $("#safeBrowsingCheck").text("Can't shorten this url because this url marked as unsafe by Google Safe Browsing ❌");
+                    var urlAndType = output.responseText.split(";");
+                    $("#safeBrowsingCheck").text(`❌ Be careful! The introduced url is marked by Google Safe browsing as a ${
+                        urlAndType[1]
+                    } website ❌`);
+                    $('#urlInput').val('');
+
                 }
 
+                console.log(`Error en peticion responsetext ${
+                    output.responseText
+                } and statuscode ${
+                    output.status
+                }`);
 
-            },
-            error: function (params) {
-                alert("Error en peticion");
             }
-
-
         });
+
 
     });
 
 
     $("#csvButton").click(function (event) {
+        $("#safeBrowsingCheck").text("");
+        $("#result").html("<div</div>");
+        event.preventDefault();
         var csv = $('#filename');
         var csvFile = csv[0].files[0];
         var ext = csv.val().split(".").pop().toLowerCase();
 
-        if($.inArray(ext, ["csv"]) === -1){
-            alert('Please upload csv');
+        if ($.inArray(ext, ["csv"]) === -1) {
+            alert('Please upload a CSV file');
             return false;
         }
-        if(csvFile != undefined){
+        if (csvFile != undefined) {
             reader = new FileReader();
-            reader.onload = function(e){
-
-                payload = {
-                    "client": {
-                        "clientId": "urlshortener-295117",
-                        "clientVersion": "1.5.2"
-                    },
-                    "threatInfo": {
-                        "threatTypes": [
-                            "THREAT_TYPE_UNSPECIFIED",
-                            "MALWARE",
-                            "SOCIAL_ENGINEERING",
-                            "UNWANTED_SOFTWARE",
-                            "POTENTIALLY_HARMFUL_APPLICATION"
-                        ],
-                        "platformTypes": ["ANY_PLATFORM"],
-                        "threatEntryTypes": ["URL"],
-                        "threatEntries": []
-                    }
-                };
-
-
+            reader.onload = function (e) {
                 csvResult = e.target.result.split("\n");
-
-                console.log(`CSV result ${csvResult}`);
-
-                for (let i = 0; i < csvResult.length; i++) {
-                    payload.threatInfo.threatEntries[i] = { url : csvResult[i] };    
-                };
-
-                
-
-                //console.log(`La payload ${JSON.stringify(payload)}`);
-
+                var urlsString = "";
+                csvResult.forEach(elem => {
+                    urlsString = urlsString + elem + ",";
+                });
                 $.ajax({
                     type: "POST",
-                    url: safeBrowsingUrl,
-                    data: JSON.stringify(payload),
-                    headers: {
-                        "Content-Type": "application/json"
+                    url: "/safeBrowsing",
+                    data: {
+                        "url": urlsString,
+                        "multiple": true
                     },
-                    dataType: 'json',
-                    beforeSend: function () {
-                        $("#safeBrowsingCheck").text("Checking all URLs against Google safe browsing ...");
+                    success: function (msg) {
+                        console.log("Successfuly checked against google safe browsing");
+                        $("#safeBrowsingCheck").text("Thess websites are verified by google safe browsing ✅");
+                        $("#notifyShortening").text("Shortening urls ... ⏳");
+                        sendMultipleUrls();
                     },
-                    success: function (output, status, xhr) {
-        
-                        console.log(`Output: ${
-                            JSON.stringify(output)
-                        } and status ${status}`);
-        
-                        if (JSON.stringify(output) == "{}") {
-                            $("#safeBrowsingCheck").text("These websites are verified by google safe browsing ✅");
-                            $("#notifyShortening").text("Shortening urls ...");
-
-
-                            var shortenedUrls = "";
-
-                            for (let i = 0; i < csvResult.length; i++) {
-                                
-                                console.log(`Sending url ${csvResult[i]}`);
-                                $.ajax({
-                                    type: "POST",
-                                    url: "/link",
-                                    data: {
-                                        "url" : csvResult[i]
-                                    },
-                                    async: false,
-                                    success: function (msg) {
-                                            console.log("Success");
-                                            if (i == csvResult.length) {
-                                                shortenedUrls = shortenedUrls.concat(msg.uri);
-                                            } else{
-                                                shortenedUrls = shortenedUrls.concat(msg.uri,",");
-                                            }
-                                    },
-                                    error: function () {
-                                        console.log("Error en peticion");
-                                        $("#result").html("<div class='alert alert-danger lead'>ERROR</div>");
-                                    }
-                                });
-                            }
-
-                            console.log(`Shortened urls ${shortenedUrls}`);
-                                
-                          
+                    error: function (output, status, xhr) {
+                        if (output.status == 404) {
+                            $("#safeBrowsingCheck").text("Sorry, Google Safe Browsing couldn't be reached 😔");
+                        } else if (output.status == 400) {
+                            $("#safeBrowsingCheck").text("Bad request, please check inserted url 🤨");
                         } else {
-                            $("#safeBrowsingCheck").text("Can't shorten some url because it is marked as unsafe by Google Safe Browsing ❌");
-                        } 
-        
-        
-                    },
-                    error: function (params) {
-                        alert("Error en peticion");
+                            var urlsAndTypes = output.responseText.split(";");
+
+                            $("#safeBrowsingCheck").text(`❌ Be careful! The introduced urls ${
+                                urlsAndTypes[0]
+                            } are marked by Google Safe browsing as a ${
+                                urlsAndTypes[1]
+                            } website ❌`);
+                            $('#urlInput').val('');
+
+                        }
                     }
-        
-        
                 });
-
-
-                
             }
-            //Asyncronous
+            // Asyncronous
             reader.readAsText(csvFile);
         }
     });
 
 });
+
+
+function sendMultipleUrls() {
+
+    var form = $('#csvForm')[0];
+    var formDataUrls = new FormData(form);
+
+    $.ajax({
+        type: "POST",
+        url: "/multiplelLinks",
+        data: formDataUrls,
+        processData: false,
+        enctype: 'multipart/form-data',
+        contentType: false,
+        success: function (msg) {
+            $("#safeBrowsingCheck").text("Thess websites are verified by google safe browsing ✅");
+            $("#notifyShortening").text("Shortening urls ... ⏳");
+            $("#notifyShortening").text("Success shortening");
+            msg.forEach(shortUrl => {
+                console.log(`Reply url: ${shortUrl}`)
+            });
+            let csvContent = "data:text/csv;charset=utf-8," + msg.map(shortUrl => shortUrl).join("\n");
+            var encodedUri = encodeURI(csvContent);
+            var link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "Short-urls.csv");
+            document.body.appendChild(link);
+            link.click();
+            $("#notifyShortening").text("File downloaded ✅");
+
+        },
+        error: function (output, status, xhr) {
+            $("#safeBrowsingCheck").text("Sorry got statuscode " + output.status);
+
+
+        }
+    });;
+}
--- src/main/java/urlshortener/web/UrlShortenerController.java
+++ src/main/java/urlshortener/web/UrlShortenerController.java
@@ -1,8 +1,33 @@
 package urlshortener.web;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.validator.routines.UrlValidator;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +36,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import urlshortener.domain.ShortURL;
 import urlshortener.service.ClickService;
 import urlshortener.service.ShortURLService;
@@ -53,6 +80,194 @@ public class UrlShortenerController {
       System.out.println(" - error");
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @RequestMapping(value = "/multiplelLinks", method = RequestMethod.POST)
+  public ResponseEntity<List<String>> multipleShortener(@RequestParam("urls") MultipartFile urls,
+      HttpServletRequest request) {
+    UrlValidator urlValidator = new UrlValidator(new String[] { "http", "https" });
+
+    try {
+      System.out.println("Received type " + urls.getContentType());
+      BufferedReader br;
+      List<String> urlsList = new ArrayList<>();
+      String line;
+      InputStream is = urls.getInputStream();
+      br = new BufferedReader(new InputStreamReader(is));
+      while ((line = br.readLine()) != null) {
+        System.out.println(line);
+        urlsList.add(line);
+      }
+      // Check if all urls are valid
+      for (String url : urlsList) {
+        if (!urlValidator.isValid(url)) {
+          return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+      }
+      List<String> resultList = new ArrayList<>();
+
+      for (String url : urlsList) {
+        ShortURL su = shortUrlService.save(url, "", request.getRemoteAddr());
+        System.out.println("Result url " + su.getUri().toString());
+        resultList.add(su.getUri().toString());
+      }
+      return new ResponseEntity<>(resultList, HttpStatus.OK);
+
+    } catch (Exception e) {
+      System.out.println("Exception  " + e.toString());
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @RequestMapping(value = "/safeBrowsing", method = RequestMethod.POST)
+  public ResponseEntity<String> safeBrowsingChecker(@RequestParam("url") String url,
+      @RequestParam(value = "multiple", required = false, defaultValue = "false") boolean multiple,
+      HttpServletRequest request) throws ClientProtocolException, IOException {
+    UrlValidator urlValidator = new UrlValidator(new String[] { "http", "https" });
+
+    String[] urlsStrings = url.split(",");
+
+    if (multiple) {
+      // Checks if all urls are valid
+      for (String tmpUrl : urlsStrings) {
+        if (!urlValidator.isValid(tmpUrl)) {
+          System.out.println("Invalid url " + tmpUrl);
+          return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+      }
+    } else {
+      if (!urlValidator.isValid(url)) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+      }
+    }
+
+    List<String> urlsList = new LinkedList<String>();
+
+    for (String tmpUrl : urlsStrings) {
+      urlsList.add(tmpUrl);
+    }
+    String json = buildPayload(urlsList);
+
+    String safeBrowsingUrl = "https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyD2-m3JAvdEYiAzPLhF-uVN2ZUIW6MkXU4";
+
+    HttpPost post = new HttpPost(safeBrowsingUrl);
+    post.addHeader("content-type", "application/json");
+    String result = "";
+    // send a JSON data
+    post.setEntity(new StringEntity(json));
+    int code = 0;
+    try (CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = httpClient.execute(post)) {
+      result = EntityUtils.toString(response.getEntity());
+      code = response.getStatusLine().getStatusCode();
+    }
+    JsonReader jsonReader = Json.createReader(new StringReader(result));
+    JsonObject jsonObject = jsonReader.readObject();
+    jsonReader.close();
+
+    if (code != 200) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /*
+     * Map<String, ?> config = new HashMap<String, Object>(); JsonBuilderFactory
+     * factory = Json.createBuilderFactory(config); JsonObject badJson =
+     * Json.createObjectBuilder() .add("matches", factory.createArrayBuilder()
+     * .add(factory.createObjectBuilder().add("threatType", "MALWARE").add("threat",
+     * factory.createObjectBuilder().add("url", url)))
+     * .add(factory.createObjectBuilder().add("threatType",
+     * "SOCIAL_ENGINEERING	").add("threat",
+     * factory.createObjectBuilder().add("url", "http://secondurl.com")))) .build();
+     */
+
+    // Not a threat
+    if (jsonObject.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.OK);
+    } else {
+      String threatType = "";
+      String threatUrl = "";
+      // A threat
+      JsonArray threats = jsonObject.getJsonArray("matches");
+      for (int i = 0; i < threats.size(); i++) {
+        String currentThreatType = threats.getJsonObject(i).getString("threatType");
+        switch (currentThreatType) {
+          case "POTENTIALLY_HARMFUL_APPLICATION":
+            currentThreatType = "Potentially harmful application";
+            break;
+          case "MALWARE":
+            currentThreatType = "Malware";
+            break;
+          case "SOCIAL_ENGINEERING":
+            currentThreatType = "Phising";
+            break;
+          case "UNWANTED_SOFTWARE":
+            currentThreatType = "Unwanted software";
+            break;
+          default:
+            currentThreatType = "Unkown";
+            break;
+        }
+
+        // Last threat
+        if (i == threats.size() - 1) {
+          threatType = threatType + currentThreatType;
+          threatUrl = threatUrl + threats.getJsonObject(i).getJsonObject("threat").getString("url");
+        } else {
+          threatType = threatType + currentThreatType + ",";
+          threatUrl = threatUrl + threats.getJsonObject(i).getJsonObject("threat").getString("url") + ",";
+
+        }
+      }
+      System.out.println("Urls " + threatUrl);
+      System.out.println("Marked as " + threatType);
+      threatUrl = threatUrl + ";" + threatType;
+
+      return new ResponseEntity<>(threatUrl, HttpStatus.FORBIDDEN);
+    }
+
+  }
+
+  private static String buildPayload(List<String> urls) {
+
+    Map<String, ?> config = new HashMap<String, Object>();
+    JsonBuilderFactory factory = Json.createBuilderFactory(config);
+    // Create Payload
+    String json = "";
+    if (urls.size() == 1) {
+      // Payload with 1 url
+      json = Json.createObjectBuilder()
+          .add("client",
+              factory.createObjectBuilder().add("clientId", "urlshortener-295117").add("clientVersion", "1.5.2"))
+          .add("threatInfo",
+              factory.createObjectBuilder()
+                  .add("threatTypes",
+                      factory.createArrayBuilder().add("THREAT_TYPE_UNSPECIFIED").add("MALWARE")
+                          .add("SOCIAL_ENGINEERING").add("UNWANTED_SOFTWARE").add("POTENTIALLY_HARMFUL_APPLICATION"))
+                  .add("platformTypes", factory.createArrayBuilder().add("ANY_PLATFORM"))
+                  .add("threatEntryTypes", factory.createArrayBuilder().add("URL")).add("threatEntries",
+                      factory.createArrayBuilder().add(factory.createObjectBuilder().add("url", urls.get(0)))))
+          .build().toString();
+
+    } else {
+      // Payload with with multiple urls
+      JsonArrayBuilder threatEntriesarray = factory.createArrayBuilder();
+      for (int i = 0; i < urls.size(); i++) {
+        threatEntriesarray.add(factory.createObjectBuilder().add("url", urls.get(i)));
+      }
+      json = Json.createObjectBuilder()
+          .add("client",
+              factory.createObjectBuilder().add("clientId", "urlshortener-295117").add("clientVersion", "1.5.2"))
+          .add("threatInfo",
+              factory.createObjectBuilder()
+                  .add("threatTypes",
+                      factory.createArrayBuilder().add("THREAT_TYPE_UNSPECIFIED").add("MALWARE")
+                          .add("SOCIAL_ENGINEERING").add("UNWANTED_SOFTWARE").add("POTENTIALLY_HARMFUL_APPLICATION"))
+                  .add("platformTypes", factory.createArrayBuilder().add("ANY_PLATFORM"))
+                  .add("threatEntryTypes", factory.createArrayBuilder().add("URL"))
+                  .add("threatEntries", threatEntriesarray))
+          .build().toString();
+    }
+    return json;
   }
 
   private String extractIP(HttpServletRequest request) {
