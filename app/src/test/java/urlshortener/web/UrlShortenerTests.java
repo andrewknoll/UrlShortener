8c9cae1b211a654556da3f8cdcffd1175b8bd6cb
elenamv13 <elenamoronvidal@hotmail.com>
2021-01-14 00:53:33 +0100

    Tests for reachable and unreachble URL

--- app/src/test/java/urlshortener/web/UrlShortenerTests.java
+++ app/src/test/java/urlshortener/web/UrlShortenerTests.java
@@ -69,55 +69,70 @@ public class UrlShortenerTests {
     this.mockMvc = MockMvcBuilders.standaloneSetup(urlShortener).build();
   }
 
-  @Ignore
   @Test
-  public void thatRedirectToReturnsTemporaryRedirectIfKeyExists() throws Exception {
-    when(shortUrlService.findByKey("someKey")).thenReturn(someUrl());
+  public void thatShortenerFailsIfTheURLisWrong() throws Exception {
+    configureSave(null, null);
 
-    mockMvc.perform(get("/{id}", "someKey")).andDo(print()).andExpect(status().isTemporaryRedirect())
-        .andExpect(redirectedUrl("http://example.com/"));
+    mockMvc.perform(post("/link").param("url", "someKey"))
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.error", is("Debe ser una URI http o https")));
   }
 
-  @Ignore
   @Test
-  public void thatRedirecToReturnsNotFoundIdIfKeyDoesNotExist() throws Exception {
-    when(shortUrlService.findByKey("someKey")).thenReturn(null);
+  public void thatShortenerSucceedsIfTheURLisCorrect() throws Exception {
+    configureSave(null, null);
 
-    mockMvc.perform(get("/{id}", "someKey")).andDo(print()).andExpect(status().isNotFound());
+    mockMvc.perform(post("/link").param("url", "https://www.google.com"))
+            .andDo(print())
+            .andExpect(status().isCreated());
   }
 
-  @Ignore
   @Test
   public void thatShortenerCreatesARedirectIfTheURLisOK() throws Exception {
     configureSave(null, null);
 
-    mockMvc.perform(post("/link").param("url", "http://example.com/")).andDo(print())
-        .andExpect(redirectedUrl("http://localhost/f684a3c4")).andExpect(status().isCreated())
-        .andExpect(jsonPath("$.hash", is("f684a3c4"))).andExpect(jsonPath("$.uri", is("http://localhost/f684a3c4")))
-        .andExpect(jsonPath("$.target", is("http://example.com/"))).andExpect(jsonPath("$.sponsor", is(nullValue())));
+    mockMvc.perform(post("/link").param("url", "http://example.com/"))
+            .andDo(print())
+            .andExpect(redirectedUrl("http://localhost/f684a3c4")).andExpect(status().isCreated())
+            .andExpect(jsonPath("$.hash", is("f684a3c4"))).andExpect(jsonPath("$.uri", is("http://localhost/f684a3c4")))
+            .andExpect(jsonPath("$.target", is("http://example.com/"))).andExpect(jsonPath("$.sponsor", is(nullValue())));
   }
 
-  @Ignore
   @Test
   public void thatShortenerCreatesARedirectWithSponsor() throws Exception {
     configureSave("http://sponsor.com/", null);
 
     mockMvc.perform(post("/link").param("url", "http://example.com/").param("sponsor", "http://sponsor.com/"))
-        .andDo(print()).andExpect(redirectedUrl("http://localhost/f684a3c4")).andExpect(status().isCreated())
-        .andExpect(jsonPath("$.hash", is("f684a3c4"))).andExpect(jsonPath("$.uri", is("http://localhost/f684a3c4")))
-        .andExpect(jsonPath("$.target", is("http://example.com/")))
-        .andExpect(jsonPath("$.sponsor", is("http://sponsor.com/")));
+            .andDo(print()).andExpect(redirectedUrl("http://localhost/f684a3c4"))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.hash", is("f684a3c4")))
+            .andExpect(jsonPath("$.uri", is("http://localhost/f684a3c4")))
+            .andExpect(jsonPath("$.target", is("http://example.com/")))
+            .andExpect(jsonPath("$.sponsor", is("http://sponsor.com/")));
   }
 
   @Ignore
   @Test
-  public void thatShortenerFailsIfTheURLisWrong() throws Exception {
+  public void thatRedirectToReturnsTemporaryRedirectIfKeyExists() throws Exception {
     configureSave(null, null);
 
-    mockMvc.perform(post("/link").param("url", "someKey")).andDo(print()).andExpect(status().isBadRequest());
+    when(shortUrlService.findByKey("f684a3c4")).thenReturn(null);
+
+    mockMvc.perform(get("/{id}", "f684a3c4"))
+            .andDo(print())
+            .andExpect(status().isTemporaryRedirect());
   }
 
-  @Ignore
+  @Test
+  public void thatRedirectToReturnsNotFoundIdIfKeyDoesNotExist() throws Exception {
+    when(shortUrlService.findByKey("someKey")).thenReturn(null);
+
+    mockMvc.perform(get("/{id}", "someKey"))
+            .andDo(print())
+            .andExpect(status().isNotFound());
+  }
+
   @Test
   public void thatShortenerFailsIfTheRepositoryReturnsNull() throws Exception {
     when(shortUrlService.save(any(String.class), any(String.class), any(String.class), anyBoolean())).thenReturn(null);
@@ -127,7 +142,7 @@ public class UrlShortenerTests {
 
   @Ignore
   @Test
-  public void thatShortenerCreatesAQRIfTheHashisStored() throws Exception {
+  public void thatShortenerCreatesAQRIfTheHashIsStored() throws Exception {
 
     ByteArrayOutputStream oos = new ByteArrayOutputStream();
     QRCode.from("http://localhost/f684a3c4").writeTo(oos);
@@ -137,10 +152,12 @@ public class UrlShortenerTests {
     when(shortUrlService.findByKey("f684a3c4"))
         .thenReturn(urlWithParameters(null, new URI("http://localhost/qr/f684a3c4")));
 
-    mockMvc.perform(get("/qr/{hash}.{format}", "f684a3c4", "png")).andDo(print()).andExpect(status().isAccepted())
-        .andExpect(header().string("hash", is("f684a3c4")))
-        .andExpect(header().string("Location", is("http://localhost/f684a3c4")))
-        .andExpect(content().bytes(oos.toByteArray()));
+    mockMvc.perform(get("/qr/{hash}.{format}", "f684a3c4", "png"))
+            .andDo(print())
+            .andExpect(status().isAccepted())
+            .andExpect(header().string("hash", is("f684a3c4")))
+            .andExpect(header().string("Location", is("http://localhost/f684a3c4")))
+            .andExpect(content().bytes(oos.toByteArray()));
   }
 
   @Ignore
@@ -153,8 +170,10 @@ public class UrlShortenerTests {
     when(shortUrlService.findByKey("f684a3c4")).thenReturn(url1());
     when(qrService.findByHash("f684a3c4")).thenReturn(qr1());
 
-    mockMvc.perform(get("/qr/{hash}", "f684a3c4")).andDo(print()).andExpect(status().isAccepted())
-        .andExpect(content().bytes(oos.toByteArray()));
+    mockMvc.perform(get("/qr/{hash}", "f684a3c4"))
+            .andDo(print())
+            .andExpect(status().isAccepted())
+            .andExpect(content().bytes(oos.toByteArray()));
   }
 
   @Ignore
@@ -162,7 +181,9 @@ public class UrlShortenerTests {
   public void thatRetrieveQRByHashReturnsNotFoundIdIfKeyDoesNotExist() throws Exception {
     when(qrService.findByHash("someKey")).thenReturn(null);
 
-    mockMvc.perform(get("/qr/{hash}", "someKey")).andDo(print()).andExpect(status().isNotFound());
+    mockMvc.perform(get("/qr/{hash}", "someKey"))
+            .andDo(print())
+            .andExpect(status().isNotFound());
   }
 
   @Test
@@ -175,18 +196,22 @@ public class UrlShortenerTests {
 
     MockMultipartFile csvFile = new MockMultipartFile("urls", null, "text/csv", csvContent.getBytes());
 
-    mockMvc.perform(MockMvcRequestBuilders.multipart("/multipleLinks").file(csvFile)).andDo(print())
-        .andExpect(status().isCreated()).andExpect(header().string("Location", is("http://localhost/f684a3c4")))
-        .andExpect(header().string("Content-Type", "text/csv;charset=ISO-8859-1"));
+    mockMvc.perform(MockMvcRequestBuilders.multipart("/multipleLinks").file(csvFile))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(header().string("Location", is("http://localhost/f684a3c4")))
+            .andExpect(header().string("Content-Type", "text/csv;charset=ISO-8859-1"));
   }
 
   @Test
   public void doesNotRedirectIfURLNotValidated() throws Exception {
     when(shortUrlService.findByKey(anyString())).thenReturn(someUnsafeUrl());
 
-    mockMvc.perform(get("/{id}", "someKey")).andExpect(status().isForbidden())
-        .andExpect(jsonPath("$.error", is("URL not yet verified")));
+    mockMvc.perform(get("/{id}", "someKey"))
+            .andExpect(status().isForbidden())
+            .andExpect(jsonPath("$.error", is("URL not yet verified")));
   }
+
 
   private void configureSave(String sponsor, URI qrUri) {
     when(shortUrlService.save(any(), any(), any(), anyBoolean()))
